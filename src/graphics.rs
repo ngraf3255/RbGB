8699165538cd6a28258f63c32f07fa8f6d47d53c
Noah <85663537+Hyphen325@users.noreply.github.com>
2025-06-14 01:53:05 -0500

    Created project skeleton

---
+++ src/graphics.rs
@@ -0,0 +1,34 @@
+#![allow(dead_code)]
+
+pub struct Screen {
+    pub width: usize,
+    pub height: usize,
+    pub buffer: Vec<u8>, // Each pixel is a byte (0-3 for Game Boy palettes)
+}
+
+impl Screen {
+    pub const WIDTH: usize = 160;
+    pub const HEIGHT: usize = 144;
+
+    pub fn new() -> Self {
+        // TODO: Initialize the screen buffer
+        unimplemented!()
+    }
+
+    pub fn clear(&mut self, _color: u8) {
+        // TODO: Clear the screen buffer with the given color
+        unimplemented!()
+    }
+
+    pub fn set_pixel(&mut self, _x: usize, _y: usize, _color: u8) {
+        // TODO: Set a pixel in the buffer
+        unimplemented!()
+    }
+
+    pub fn get_pixel(&self, _x: usize, _y: usize) -> u8 {
+        // TODO: Get a pixel from the buffer
+        unimplemented!()
+    }
+
+    // TODO: Add more methods for drawing, sprites, etc.
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+#![allow(dead_code)]
+use crate::types::*;
+
+mod cpu;
+mod graphics;
+mod sound;
+mod types;
+
+///Main entry point to gameboy simulation
 fn main() {
     println!("Hello, world!");
     println!("I am testing the workflow demo!!!")
+}
+
+struct Emulator {
+    cartridge: cartridge_memory,
+}
+
+impl Emulator {
+    /// Calls the needed functions once a frame
+    fn update() {
+        let _cycle: Byte = 0;
+    }
 }
