07bf89db7ebca3a971c06f09a8b231952bc5fe5f
Noah Graf <noah@grafhome.net>
2025-06-15 18:42:55 -0500

    Banking updates Part 2

--- src/mem.rs
+++ src/mem.rs
@@ -3,35 +3,13 @@ use std::ops::BitAnd;
 /// Functions and storage for operating on device memory
 use crate::types::*;
 
-#[derive(PartialEq)]
-pub enum RomBankingType {
-    MBC1,
-    MBC2,
-    None,
-}
-
-#[derive(Clone, Copy)]
-pub enum CurrentRomBank {
-    Bank1 = 1,
-    Bank2 = 2,
-    Bank3 = 3,
-    Bank4 = 4,
-}
-
-#[derive(Clone, Copy)]
-pub enum CurrentRamBank {
-    Bank0,
-    Bank1,
-    Bank2,
-    Bank3,
-}
-
 pub struct Memory {
     pub mem: Ram,
     rom_banking_type: RomBankingType,
     rom_banks: CurrentRomBank,
     ram_banks: CurrentRamBank,
     ram_write_enable: bool,
+    rom_bank_enable: bool,
 }
 
 impl Memory {
@@ -42,6 +20,7 @@ impl Memory {
             rom_banks: CurrentRomBank::Bank1,
             ram_banks: CurrentRamBank::Bank0,
             ram_write_enable: false,
+            rom_bank_enable: false,
         }
     }
 
@@ -50,14 +29,12 @@ impl Memory {
         // are we reading from the rom memory bank?
         if (0x4000..0x7FFF).contains(&addr) {
             let addr = addr as usize;
-            return self.mem[addr + ((self.rom_banks as usize)*0x4000)] ;
+            return self.mem[addr + ((self.rom_banks as usize) * 0x4000)];
+        } else if (0xA000..=0xBFFF).contains(&addr) {
+            // RAM bank storage and indexing
+            let addr = addr as usize;
+            return self.mem[addr + (self.ram_banks as usize) * 0x2000];
         }
-
-        else if (0xA000..=0xBFFF).contains(&addr) {
-        // RAM bank storage and indexing
-        let addr = addr as usize;
-        return self.mem[addr + (self.ram_banks as usize) * 0x2000];
-    }
         // Else return memory
         self.mem[addr as usize]
     }
@@ -67,6 +44,13 @@ impl Memory {
         // this is read only memory and should not be written to
         if addr < 0x8000 {
             self.handle_banking(addr, value);
+        }
+        //inserts value into the ram banks if enabled
+        else if (0xA000..0xC000).contains(&addr) {
+            if self.ram_write_enable {
+                let addr = addr as usize;
+                self.mem[addr + (self.ram_banks as usize * 0x2000)] = value;
+            }
         }
         // echo ram writes to two locations
         else if (0xE000..0xFE00).contains(&addr) {
@@ -82,46 +66,99 @@ impl Memory {
     }
 
     fn handle_banking(&mut self, addr: Word, value: Byte) {
-
         // Performs a ram bank change
         if addr < 0x2000 {
             if self.rom_banking_type != RomBankingType::None {
                 self.enable_ram_banking(addr, value);
             }
-        } 
-
+        }
         // Performas a ROM bank change
         else if (0x2000..0x4000).contains(&addr) {
             if self.rom_banking_type != RomBankingType::None {
                 self.change_low_rom_banking(value);
             }
         }
-
         // Performs a rom or ram bank change
         else if (0x4000..0x6000).contains(&addr) {
-
+            if self.rom_banking_type == RomBankingType::MBC1 {
+                // if in rom banking mode, set the rom bank value
+                // otherwise modify ram banking
+                if self.rom_bank_enable {
+                    self.change_high_rom_banking(value);
+                } else {
+                    self.change_ram_banking(value);
+                }
+            }
+        }
+        // Now handle whether we are rom banking or ram banking
+        else if (0x6000..0x8000).contains(&addr) {
+            if self.rom_banking_type == RomBankingType::MBC1 {
+                self.change_banking_mode(value);
+            }
         }
     }
 
     fn enable_ram_banking(&mut self, addr: Word, value: Byte) {
-        if self.rom_banking_type == RomBankingType::MBC2 {
-            if self.read_byte(addr).bitand(0x10) == 0x10 {
-                return;
-            }
+        // If the mdoe is MBC2 we don't need to change anything
+        if self.rom_banking_type == RomBankingType::MBC2
+            && self.read_byte(addr).bitand(0x10) == 0x10
+        {
+            return;
         }
+
         // Checks and sets ram enablement
         if value.bitand(0xF) == 0xA {
             self.ram_write_enable = true;
-        }else if value.bitand(0xF) == 0x0 {
+        } else if value.bitand(0xF) == 0x0 {
             self.ram_write_enable = false;
         }
     }
 
-    fn change_low_rom_banking(&mut self, value: Byte) {
+    fn change_ram_banking(&mut self, value: Byte) {
+        self.ram_banks = match value {
+            0 => CurrentRamBank::Bank0,
+            1 => CurrentRamBank::Bank1,
+            2 => CurrentRamBank::Bank2,
+            3 => CurrentRamBank::Bank3,
+            _ => CurrentRamBank::Bank0,
+        }
+    }
 
+    fn change_low_rom_banking(&mut self, value: Byte) {
+        if self.rom_banking_type == RomBankingType::MBC2 {
+            self.rom_banks = CurrentRomBank::from(value & 0xF);
+            return;
+        }
+
+        //turns off the lower 5 bits of the banking mode
+        let lower5: Byte = value & 31;
+        let current = self.rom_banks as u8;
+        let masked = (current & 224) | lower5;
+        self.rom_banks = CurrentRomBank::from(masked);
+        if self.rom_banks == CurrentRomBank::Bank0 {
+            self.rom_banks = CurrentRomBank::Bank1;
+        }
     }
     fn change_high_rom_banking(&mut self, value: Byte) {
+        //turns off the upper 3 bits of the banks and the lower 5 of the data
+        let current = self.rom_banks as u8;
+        let masked = (current & 31) | (value & 224);
+        self.rom_banks = CurrentRomBank::from(masked);
+        if self.rom_banks == CurrentRomBank::Bank0 {
+            self.rom_banks = CurrentRomBank::Bank1;
+        }
+    }
 
+    fn change_banking_mode(&mut self, value: Byte) {
+        self.rom_bank_enable = match value & 0x1 {
+            0 => true,
+            1 => false,
+            _ => true,
+        };
+
+        if self.rom_bank_enable {
+            self.ram_banks = CurrentRamBank::Bank0;
+        }
     }
 
     /// Returns the rom banking type of the current game
