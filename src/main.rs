6e124416334a98398947b582a5bacd3654f31429
Noah Graf <noah@grafhome.net>
2025-11-16 22:08:09 -0600

    Main overhaul

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ mod sdl;
 
 ///Main entry point to gameboy simulation
 fn main() -> Result<(), String> {
-    println!("Hello, world!");
+    println!("Starting emulator");
 
     let mut emulator = Emulator::new();
     let mut sdl_app = SdlApp::new()?;
